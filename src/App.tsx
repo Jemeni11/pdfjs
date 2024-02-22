2ef617f5753a48dd37f2147cf49a06fcb7f400f1
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-02-22 13:12:17 +0100

    Refactor App component to handle PDF file selection and extract text

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import * as pdfjsLib from "pdfjs-dist";
+import pdfjsWorker from "pdfjs-dist/build/pdf.worker?worker";
+import type { TextItem } from "pdfjs-dist/types/src/display/api";
+
+pdfjsLib.GlobalWorkerOptions.workerPort = new pdfjsWorker();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [hasFileBeenSelected, setHasFileBeenSelected] = useState(false);
 
-  return (
+  function readArrayBufferFromFile(file: File) {
+    const reader = new FileReader();
+    reader.onload = async function (event) {
+      const arrayBuffer = event.target!.result!;
+      if (arrayBuffer instanceof ArrayBuffer) {
+        const loadingTask = pdfjsLib.getDocument(arrayBuffer);
+        return loadingTask.promise.then(async function (pdf) {
+          const totalPageCount = pdf.numPages;
+          const countPromises = [];
+          for (
+            let currentPage = 1;
+            currentPage <= totalPageCount;
+            currentPage++
+          ) {
+            const page = pdf.getPage(currentPage);
+            countPromises.push(
+              page.then(async function (page) {
+                const textContent = page.getTextContent();
+                const text = await textContent;
+                return text.items.map(function (s) {
+                  const textItem = s as TextItem;
+                  console.log(textItem.str);
+                  return textItem.str;
+                });
+              })
+            );
+          }
+          const texts = await Promise.all(countPromises);
+          console.log(texts);
+        });
+      }
+    };
+    reader.readAsArrayBuffer(file);
+  }
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setHasFileBeenSelected(true);
+    readArrayBufferFromFile(event.target.files![0]);
+  }
+
+  return hasFileBeenSelected ? (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Worked!</p>
     </>
-  )
+  ) : (
+    <>
+      <label htmlFor="pdf_input">Select a pdf file</label>
+      <input
+        type="file"
+        id="pdf_input"
+        name="pdf_input"
+        accept=".pdf"
+        style={{ opacity: 0 }}
+        onChange={handleInputChange}
+      />
+    </>
+  );
 }
 
-export default App
+export default App;
